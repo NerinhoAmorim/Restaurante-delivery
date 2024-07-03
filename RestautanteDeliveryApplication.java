d5e68196de52305f6eacdbf9e8e71c66f9f33a9a
Nerinho Nascimento <neramornas@gmail.com>
2024-07-03 13:42:33 -0300

    implementa handler(correcao)
    Signed-off-by: Nerinho Nascimento <neramornas@gmail.com>

---
+++ RestautanteDeliveryApplication.java
@@ -0,0 +1,13 @@
+package dev.wakandaacademy.desafio2.restautante_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestautanteDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestautanteDeliveryApplication.class, args);
+	}
+
+}
