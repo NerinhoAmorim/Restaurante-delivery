40b73a3ec9b82c3733e8b589dd28e038ed4da482
Nerinho Nascimento <neramornas@gmail.com>
2024-07-03 15:31:34 -0300

    first commit
    Signed-off-by: Nerinho Nascimento <neramornas@gmail.com>

---
+++ src/test/java/dev/wakandaacademy/desafio2/restautante_delivery/RestautanteDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wakandaacademy.desafio2.restautante_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestautanteDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
